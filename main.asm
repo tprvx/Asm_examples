261cfa17ae02cd59598e2598e7b2c48c96fb9c03
vm4 <vm4@vm4.vm4>
2023-08-17 19:39:09 -0400

    Hello world

---
+++ main.asm
@@ -0,0 +1,54 @@
+format ELF64 executable
+entry _start
+
+endl equ 0xA
+
+msg db "Hello World", endl, 0
+
+_start:
+	mov rax, msg
+	call print_str
+	call exit
+
+;| input
+; rax = string
+;| output
+; rax = lenght
+get_lenght:
+	push rdx
+	xor rdx, rdx
+	.next_iter:
+		cmp [rax+rdx], byte 0
+		je .close
+		inc rdx
+		jmp .next_iter
+	.close:
+		mov rax, rdx
+		pop rdx
+ 		ret
+
+;| input
+; rax = string
+print_str:
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	mov rcx, rax
+	call get_lenght
+	mov rdx, rax
+	mov rax, 4
+	xor rbx, rbx
+	inc rbx
+	int 0x80
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+	ret
+
+exit:
+	xor rax, rax
+	inc rax
+	mov rbx, 0
+	int 0x80
